65b99f05c1aefe143f7c50005509778f8fadf02a
Samuli Salin <samulsal@metropolia.fi>
2020-09-30 10:39:15 +0300

    Simulation controller added

---
+++ Controllers/SimulationController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASG_Leaderboard_Project.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SimulationController : ControllerBase
+    {
+        private readonly MongoRepository _repo;
+
+        public SimulationController(MongoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // ------------ SIMULATION FUNCTIONS----------------------
+    }
