b022b3fc32143dd112f6ee6f814d3f15b68ca9d8
Samuli Salin <samulsal@metropolia.fi>
2020-10-01 13:10:23 +0300

    New error folder

---
+++ CustomErrors/OutOfRangeError.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace ASG_Leaderboard_Project
+{
+    [Serializable]
+    internal class OutOfRangeError : Exception
+    {
+        public OutOfRangeError()
+        {
+        }
+
+        public OutOfRangeError(string message) : base(message)
+        {
+        }
+
+        public OutOfRangeError(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OutOfRangeError(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
